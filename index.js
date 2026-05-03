0120065a75304edaf5799f8b7c9991dfcac04834
wumeibomb <aminakavele@gmail.com>
2026-05-04 02:07:31 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 async function fetchWORD() {
     try {
-        const tryout = document.getElementById("wordsearch").value.toLowerCase()
+        const tryout = document.getElementById("wordsearch").value
         const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${tryout}`)
         const data = await response.json()
         //console.log(data)
@@ -14,42 +14,44 @@ async function fetchWORD() {
         const world = data[0].word
         const wordMeaning = data[0].meanings[0].definitions[0].definition
         const audio = data[0].phonetics[0].audio //only american pronounciation
-        const exampleusage = data[0].meanings[2].definitions[1].example
-        const exampleusage2 = data[0].meanings[1].definitions[0].example
-        const moredefs1 = ""
-        const moredefs2 = ""
+        const exampleusage = data[0].meanings[1].definitions[1].example
+        const exampleusage2 = data[0].meanings[0].definitions[0].example
+        const exampleusage3 = data[0].meanings[2].definitions[0].example
 
+        if(exampleusage == undefined){
+        const Example = document.getElementById("example").innerHTML =  "<h3>Not available ᨳ𐔌՞҂ ˕ ֊՞𐦯ᜊ </h3>"
         const SearchedWord = document.getElementById("SearchedWord").innerHTML = world
         const wordDesc = document.getElementById("wordDesc").innerHTML = wordMeaning
         const AUDIO = document.getElementById("audio").src = audio
-        const Example = document.getElementById("example").innerHTML =  exampleusage 
-    const Example2 = document.getElementById()
-        if(exampleusage == undefined || exampleusage == ""){
-                 Example = document.getElementById("example").innerHTML =  "<h2> Not available ᨳ𐔌՞҂ ˕ ֊՞𐦯ᜊ </h2>" 
-                 return
+        const Example2 = document.getElementById("example2").innerHTML = exampleusage2
+        const Example3 = document.getElementById("example3").innerHTML =  "<h3>Not available ᨳ𐔌՞҂ ˕ ֊՞𐦯ᜊ </h3>"
+
         }
-        const renderwordDetails = (word) => {
-    return `
-    <div id = "wordContainer" class = "wordCont">
-     <div class = "wordDesc">
-        <h3> ${SearchedWord} </h3>
-        <h2> ${wordDesc} </h2>
-        <audio controls>
-            <source src ="${AUDIO}" type="audio/mpeg">
-        </audio>
-     </div>
-     <div id = "morecontent">
-        <button id = "moremore"> more definitions</button>
-        <h3 id="${Example}">bruh </h3>
-        </div>
-     `
-     
+        if (exampleusage2 == undefined) {
+        const Example = document.getElementById("example").innerHTML =  exampleusage
+        const SearchedWord = document.getElementById("SearchedWord").innerHTML = world
+        const wordDesc = document.getElementById("wordDesc").innerHTML = wordMeaning
+        const AUDIO = document.getElementById("audio").src = audio
+        const Example2 = document.getElementById("example2").innerHTML = "<h3>Not available ᨳ𐔌՞҂ ˕ ֊՞𐦯ᜊ </h3>"
+        }
+        // words like dream and hello aren't working yet...
+        if (exampleusage == undefined && exampleusage2 == undefined) {
+        const Example = document.getElementById("example").innerHTML =  "<h3>Not available ᨳ𐔌՞҂ ˕ ֊՞𐦯ᜊ </h3>"
+        const SearchedWord = document.getElementById("SearchedWord").innerHTML = world
+        const wordDesc = document.getElementById("wordDesc").innerHTML = wordMeaning
+        const AUDIO = document.getElementById("audio").src = audio
+        const Example2 = document.getElementById("example2").innerHTML = "<h3>Not available ᨳ𐔌՞҂ ˕ ֊՞𐦯ᜊ </h3>"
+        const Example3 = document.getElementById("example2").innerHTML = exampleusage3
+        }
 }
-    }
     catch (error){
         console.error("ITS ME!", error)
     }
-
+    
 }
 
+async function fetchDefs() {
+    //will update this sorrryyyyy
+    //words like speak show a not so common definition...
+}
 
