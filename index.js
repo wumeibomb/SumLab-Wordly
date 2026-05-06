3df0fe8fb9e909da72cb95b97e329dccf8710049
wumeibomb <aminakavele@gmail.com>
2026-05-06 12:27:15 +0400

    Updated README and JavaScript

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Search functionality, display search results for the word
-// audio playback fro pronounciation???
-//fetch data from api, pase and display fetched, unser input and events
+// audio playback for pronounciation???
+//fetch data from api, parse and display fetched, user input and events
 
 
 async function fetchWORD() {
@@ -35,7 +35,7 @@ async function fetchWORD() {
         const Example2 = document.getElementById("example2").innerHTML = "<h3>Not available ᨳ𐔌՞҂ ˕ ֊՞𐦯ᜊ </h3>"
       const Example3 = document.getElementById("example3").innerHTML = exampleusage3 
         }
-        // words like cute and dream aren't working, I'm not sure why the 2nd else-if function isn't applying to them
+        // words like cute and dream aren't working correctly, I'm not sure why the else-if functions aren't applying to them
 
      if (exampleusage3 == undefined) {
         const Example = document.getElementById("example").innerHTML =  exampleusage 
@@ -64,25 +64,25 @@ async function fetchWORD() {
         const Example3 = document.getElementById("example3").innerHTML = "<h3>Not available ᨳ𐔌՞҂ ˕ ֊՞𐦯ᜊ </h3>"
         }
         else if (exampleusage == undefined && exampleusage2 == undefined && exampleusage3 == undefined){
-        const Example = document.getElementById("example").innerHTML =  exampleusage
-        const SearchedWord = document.getElementById("SearchedWord").innerHTML = world
-        const wordDesc = document.getElementById("wordDesc").innerHTML = wordMeaning
-        const AUDIO = document.getElementById("audio").src = audio
-        const Example2 = document.getElementById("example2").innerHTML = exampleusage2 
-        const Example3 = document.getElementById("example2").innerHTML = exampleusage3 
-
-        }
-        else {
         const Example = document.getElementById("example").innerHTML =  "<h3>Not available ᨳ𐔌՞҂ ˕ ֊՞𐦯ᜊ </h3>"
         const SearchedWord = document.getElementById("SearchedWord").innerHTML = world
         const wordDesc = document.getElementById("wordDesc").innerHTML = wordMeaning
         const AUDIO = document.getElementById("audio").src = audio
         const Example2 = document.getElementById("example2").innerHTML = "<h3>Not available ᨳ𐔌՞҂ ˕ ֊՞𐦯ᜊ </h3>" 
-        const Example3 = document.getElementById("example3").innerHTML = "<h3>Not available ᨳ𐔌՞҂ ˕ ֊՞𐦯ᜊ </h3>" 
+        const Example3 = document.getElementById("example2").innerHTML = "<h3>Not available ᨳ𐔌՞҂ ˕ ֊՞𐦯ᜊ </h3>" 
+
+        }
+        else {
+        const Example = document.getElementById("example").innerHTML =  exampleusage
+        const SearchedWord = document.getElementById("SearchedWord").innerHTML = world
+        const wordDesc = document.getElementById("wordDesc").innerHTML = wordMeaning
+        const AUDIO = document.getElementById("audio").src = audio
+        const Example2 = document.getElementById("example2").innerHTML = exampleusage2
+        const Example3 = document.getElementById("example3").innerHTML = exampleusage3
         }
 }
     catch (error){
-        console.error("ITS ME!", error)
+        console.error("ERROR FOUNDDDD", error)
     }
     
 }
